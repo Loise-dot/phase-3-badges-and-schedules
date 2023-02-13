c3a315431d5003453e7fbee8c5a9c84a586acfb9
Loise <loise.wambui@student.moringaschool.com>
2023-02-13 20:57:50 +0300

    Done

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+
+end
+badge_maker("Arel")
+
+
+  
+  def batch_badge_creator(speakers)
+    speakers.map do |speaker|
+      "Hello, my name is #{speaker}."
+    end
+  end
+
+  def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room|
+    "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+  end
+  
+  
+  def printer(speakers)
+    batch_badge_creator(speakers).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(speakers).each do |assignment|
+      puts assignment
+    end
+  end
